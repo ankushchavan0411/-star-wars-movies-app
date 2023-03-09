504ac05b2afb2e2846eedf0bb6bba367da3836ca
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-09 19:57:58 +0530

    AC | handle Search On Change

--- src/components/Filters/SearchBox.jsx
+++ src/components/Filters/SearchBox.jsx
@@ -3,10 +3,14 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import { useState } from "react";
 
 const SearchBox = () => {
+  const [value, setValue] = useState("");
+
   const handleSearchOnChange = (e) => {
     console.log("e", e?.target?.value);
+    setValue(e?.target?.value);
   };
   return (
     <InputGroup>
@@ -14,9 +18,10 @@ const SearchBox = () => {
         placeholder="Search movie"
         onChange={handleSearchOnChange}
         size="lg"
+        value={value}
       />
-      <Button variant="primary" id="search">
-        Search
+      <Button variant="warning" id="search" onClick={() => setValue("")}>
+        Clear Search
       </Button>
     </InputGroup>
   );
