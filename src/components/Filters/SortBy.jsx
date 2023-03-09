5ccc2e528e0b782d007b51b04e3ca65f6ce1674d
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-09 19:57:36 +0530

    AC | Added SortBy Components

---
+++ src/components/Filters/SortBy.jsx
@@ -0,0 +1,22 @@
+/** @format */
+
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+
+const SortBy = () => {
+  const [value, setValue] = useState("default");
+  const handleSelectOnChange = (e) => {
+    console.log("eeee", e.target.value);
+    setValue(e?.target?.value);
+  };
+  return (
+    <Form.Select size="lg" onChange={handleSelectOnChange} value={value}>
+      <option value="default">Sort by...</option>
+      <option value="all">All</option>
+      <option value="year">Year</option>
+      <option value="episode">Episode</option>
+    </Form.Select>
+  );
+};
+
+export default SortBy;
--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -2,11 +2,21 @@
 
 import React from "react";
 import SearchBox from "./SearchBox";
+import SortBy from "./SortBy";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Filters = () => {
   return (
     <React.Fragment>
-      <SearchBox />
+      <Row>
+        <Col className="col-3">
+          <SortBy />
+        </Col>
+        <Col>
+          <SearchBox />
+        </Col>
+      </Row>
     </React.Fragment>
   );
 };
