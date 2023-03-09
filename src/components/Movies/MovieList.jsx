936bc21085f33bcf6d3e36eeb084b646db4eb8fe
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-10 01:49:19 +0530

    AC | Added card in movie list

--- src/components/Movies/MovieList.jsx
+++ src/components/Movies/MovieList.jsx
@@ -1,14 +1,20 @@
 /** @format */
 
 import DataTable from "../Common/DataTable";
-
+import Card from "react-bootstrap/Card";
 import React from "react";
 import { DataNotFound } from "../Common/DataNotFound";
 
 const MovieList = ({ movies }) => {
   return (
     <React.Fragment>
-      {movies.length > 0 ? <DataTable movieList={movies} /> : <DataNotFound />}
+      {movies.length > 0 ? (
+        <DataTable movieList={movies} />
+      ) : (
+        <Card style={{ height: "40vh" }}>
+          <DataNotFound />
+        </Card>
+      )}
     </React.Fragment>
   );
 };
