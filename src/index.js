2f96811b095c8810276281f717a209b10d695a1a
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-09 23:44:13 +0530

    AC | Added Provider

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-/** @format */
+/**
+ * @format
+ * @author Ankush Chavan
+ * @description This is main root file of app
+ */
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import App from "./App";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
