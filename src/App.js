11c21518427b27d18c5bf8c1a7337858ff382a7a
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-09 23:44:35 +0530

    AC | dispacth getMovies API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-/** @format */
+/**
+ * @format
+ * @author Ankush Chavan
+ * @description Here we are combining all our components.
+ */
 
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Movies from "./components/Movies";
+import { useDispatch } from "react-redux";
+import { getMovies } from "./redux/action";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMovies());
+  });
+
   return (
     <React.Fragment>
       <Header />
