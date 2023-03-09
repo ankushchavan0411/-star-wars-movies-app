dcce7e02ea5212bbad913723a64aa8cb34050023
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-09 20:15:18 +0530

    AC | Added DataNotFound Components

---
+++ src/components/Common/DataNotFound.jsx
@@ -0,0 +1,5 @@
+/** @format */
+
+export const DataNotFound = ({ text = "Data not found" }) => (
+  <p className="text-center mt-5">{text}</p>
+);
--- src/components/Movies/MovieDetails.jsx
+++ src/components/Movies/MovieDetails.jsx
@@ -1,6 +1,18 @@
 /** @format */
 
+import React, { useState } from "react";
+import { DataNotFound } from "../Common/DataNotFound";
+
 const MovieDetails = () => {
-  return <h1>Movie Details</h1>;
+  const [movie, setMovie] = useState([]);
+  return (
+    <React.Fragment>
+      {movie.length > 0 ? (
+        <h1>Movie Details</h1>
+      ) : (
+        <DataNotFound text={"No movie selected"} />
+      )}
+    </React.Fragment>
+  );
 };
 export default MovieDetails;
