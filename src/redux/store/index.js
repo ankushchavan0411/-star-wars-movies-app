6cd327901ed1c3bfbc07de10867183e76630f6d0
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-09 23:42:25 +0530

    AC | create store

---
+++ src/redux/store/index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ * @author Ankush Chavan
+ * @description Here we have create redux store
+ */
+
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "../reducer";
+
+export default createStore(rootReducer, applyMiddleware(thunk));
