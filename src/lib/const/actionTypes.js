daa223c6ada71fe3f5f70077a3bfc476e10221d4
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-09 23:41:41 +0530

    AC | Added action types

---
+++ src/lib/const/actionTypes.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ * @author Ankush Chavan
+ * @description Here we have kept all action types
+ */
+
+export const LOADING_STARTED = "LOADING_STARTED";
+export const LOADING_STOP = "LOADING_STOP";
+
+export const GET_MOVIES_SUCCESS = "GET_MOVIES_SUCCESS";
