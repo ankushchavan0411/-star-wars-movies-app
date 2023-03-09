3ac95809578926249bff50b478bcb5ea364857de
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-09 19:07:27 +0530

    AC | Added Movie List components

---
+++ src/components/Movies/MovieList.jsx
@@ -0,0 +1,6 @@
+/** @format */
+
+const MovieList = () => {
+  return <h1>Movie List</h1>;
+};
+export default MovieList;
--- src/components/Movies/index.jsx
+++ src/components/Movies/index.jsx
@@ -3,12 +3,15 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import MovieList from "./MovieList";
 
 const Movies = () => {
   return (
     <Container>
       <Row>
-        <Col>1 of 2</Col>
+        <Col>
+          <MovieList />
+        </Col>
         <Col>2 of 2</Col>
       </Row>
     </Container>
