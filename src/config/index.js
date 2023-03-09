b4d5a1ad1d90d42559a71e35e399344708287649
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-09 18:11:08 +0530

    AC | Added config file and in that add env end point and footer text

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ * @author Ankush Chavan
+ * @description In this file we configure footerText, newsCategory, apiKey and baseUrl
+ */
+
+module.exports = {
+  apiKey: process.env.REACT_APP_END_POINT,
+  footerText: "Star Wars Movies  © 2023",
+};
