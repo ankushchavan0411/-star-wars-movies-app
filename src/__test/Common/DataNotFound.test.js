278eb85746408cfc2590443e62be0700eb7b21fd
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-11 16:56:40 +0530

    AC | update add new test text renders with props

--- src/__test/Common/DataNotFound.test.js
+++ src/__test/Common/DataNotFound.test.js
@@ -3,8 +3,15 @@
 import { render, screen } from "@testing-library/react";
 import { DataNotFound } from "../../components/Common/DataNotFound";
 
-test("renders data-not-found p tag to display no data found", () => {
+test("Check wehter P tag is render", () => {
   render(<DataNotFound />);
   const linkElement = screen.getByTestId("data-not-found");
+  expect(linkElement).not.toBeNull();
+});
+
+test("text renders with props", () => {
+  const message = "Opps..! movies not found";
+  render(<DataNotFound text={message} />);
+  const linkElement = screen.getByText(message);
   expect(linkElement).toBeInTheDocument();
 });
