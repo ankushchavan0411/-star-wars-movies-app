85fed0153d6a3b5410532eb1b1ec6e91db479432
Ankush Chavan <ankushchavan0411@gmail.com>
2023-03-12 14:21:48 +0530

    AC | update footer text

--- src/__test/Footer.test.js
+++ src/__test/Footer.test.js
@@ -2,9 +2,10 @@
 
 import { render, screen } from "@testing-library/react";
 import Footer from "../components/Footer";
+import { footerText } from "../config";
 
 test("renders Star Wars Movies © 2023 footer text", () => {
   render(<Footer />);
-  const linkElement = screen.getByText(/Star Wars Movies © 2023/i);
+  const linkElement = screen.getByText(footerText);
   expect(linkElement).toBeInTheDocument();
 });
